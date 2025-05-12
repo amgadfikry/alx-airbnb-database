83b8efd5bd6840f1ff0888b850cb76c57795762d
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-05-12 18:52:53 +0300

    create aggreate stament to test database

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,14 @@
+-- a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT u.id, u.first_name, u.last_name, COUNT(b.id) AS total_bookings
+FROM users u
+LEFT JOIN bookings b
+ON u.id = b.user_id
+GROUP BY u.id, u.first_name, u.last_name;
+
+-- a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+SELECT p.id, p.name, COUNT(b.id) AS total_bookings, ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS rank
+FROM properties p
+LEFT JOIN bookings b
+ON p.id = b.property_id
+GROUP BY p.id, p.name
+ORDER BY rank;
