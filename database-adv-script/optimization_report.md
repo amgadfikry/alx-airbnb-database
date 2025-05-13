1750276c894686a23cab35f186472c4b577de1ac
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-05-13 11:27:10 +0300

    optimize complex queries

---
+++ database-adv-script/optimization_report.md
@@ -0,0 +1,1 @@
+# Optimize Complex Queries
---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,9 @@
+-- Query that retrieves all bookings along with the user details, property details, and payment details
+
+EXPLAIN ANALYZE
+SELECT b.id, u.first_name, u.last_name, p.name, p.type, py.payment_method, py.status
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.id
+INNER JOIN properties p ON b.property_id = p.id
+INNER JOIN payments py ON b.id = py.booking_id
+ORDER BY b.id;
